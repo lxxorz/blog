92283880f4b0f5aaf35e8a59e58f2f71995f779e
bjorn <lxxorz@gmail.com>
2023-08-07 17:54:59 +0800

    docs:update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vitepress'
 
+type SideItem = {
+  text: string
+  link: string
+  children?: SideItem[]
+}
+const globalPrefix=(prefix: string[] | string, side_items: SideItem[])=>{
+  const prefixArr = typeof prefix === 'string' ? [prefix] : prefix
+  return side_items.map(item => {
+    item.link = [...prefixArr, item.link].join('/')
+    if (item.children) {
+      item.children = globalPrefix(prefix, item.children)
+    }
+    return item
+  })
+}
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "BJORN",
+  title: "computer science docs",
   description: "docs about cs",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
@@ -14,10 +29,10 @@ export default defineConfig({
     sidebar: [
       {
         text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
+        items: globalPrefix("docs/cs", [
+          { text: '撤销回退-如何找回正确的历史记忆', link: '/undo-redo' },
+          {text: '非常推荐的几个编程频道', link: '/programming-channels'},
+        ])
       }
     ],
 
---
+++ docs/cs/programming-channels.md
@@ -0,0 +1,11 @@
+---
+outline: deep
+---
+
+# 非常推荐的编程频道
+
+## Youtube
+
+### Code Aesthetic
+频道的视频不多，但每一个视频都对写出可维护的、优雅的代码有指导性作用
+https://www.youtube.com/@CodeAesthetic
