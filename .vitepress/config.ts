e389fa1003007653cc7f1fab580e73af2a177dfc
bjorn <lxxorz@gmail.com>
2023-08-12 19:54:42 +0800

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -15,29 +15,33 @@ const globalPrefix=(prefix: string[] | string, side_items: SideItem[])=>{
     return item
   })
 }
+
+const latest = globalPrefix("docs/cs", [
+  { text: '撤销回退-如何找回正确的历史记忆', link: '/undo-redo' },
+  {text: '非常推荐的几个编程频道', link: '/programming-channels'},
+])
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "computer science docs",
   description: "docs about cs",
+  lastUpdated: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Latest', link: '/' },
     ],
-
     sidebar: [
       {
-        text: 'Examples',
-        items: globalPrefix("docs/cs", [
-          { text: '撤销回退-如何找回正确的历史记忆', link: '/undo-redo' },
-          {text: '非常推荐的几个编程频道', link: '/programming-channels'},
-        ])
+        text: 'Web',
+        items: latest
       }
     ],
-
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/lxxorz.git' }
+    ],
+    search: {
+      provider: 'local'
+    }
   }
 })
--- index.md
+++ index.md
@@ -4,22 +4,9 @@ layout: home
 
 hero:
   name: "BJORN"
-  text: "docs about cs"
-  tagline: My great project tagline
+  tagline: "dadfsadf"
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
-
-features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      text: Home
+      link: /docs/cs/undo-redo.md
 ---
-
